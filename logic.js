abb30d9f261c6c0ddace9b5bbb78dd9800c028cc
ahmad.mer <ahmad.mer@gmail.com>
2019-01-07 23:08:21 -0800

    updated

--- logic.js
+++ logic.js
@@ -1,12 +1,8 @@
 // Store our API endpoint inside queryUrl
-// var queryUrl = "http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=" +
-//   "2014-01-02&maxlongitude=-69.52148437&minlongitude=-123.83789062&maxlatitude=48.74894534&minlatitude=25.16517337";
 var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
-
 
 // Perform a GET request to the query URL
 d3.json(queryUrl, function(data) {
-  // console.log("Hello")
   // Once we get a response, send the data.features object to the createFeatures function
   createFeatures(data.features);
 });
@@ -16,14 +12,31 @@ function createFeatures(earthquakeData) {
   // Define a function we want to run once for each feature in the features array
   // Give each feature a popup describing the place and time of the earthquake
   function onEachFeature(feature, layer) {
-    layer.bindPopup("<h3>" + feature.properties.place +
+    layer.bindPopup("<h3>" + feature.properties.place + feature.properties.mag +
       "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
   }
 
+  function markerSize(earthquakes) {
+    return earthquakes / 10;
+  }
+  function Color(i) {
+    return i > 7 ?  '#e6f598' :
+           i > 6  ?  '#66c2a5':
+           i > 5  ?  '#3288bd' :
+           i > 4  ? '#abdda4' :
+           i > 3   ? '#5e4fa2' :
+           i > 2   ? '#ffffbf' :
+           i > 1   ? '#fee08b' :
+           i > 0   ? '#fdae61' :
+                     "#ff7800"
+  }
   // Create a GeoJSON layer containing the features array on the earthquakeData object
   // Run the onEachFeature function once for each piece of data in the array
   var earthquakes = L.geoJSON(earthquakeData, {
-    onEachFeature: onEachFeature
+    onEachFeature: onEachFeature,
+    adjustlayer: function(feature, latlng){
+        return L.circleMaker(latlng)
+    }
   });
 
   // Sending our earthquakes layer to the createMap function
@@ -47,10 +60,6 @@ function createMap(earthquakes) {
     accessToken: API_KEY
   });
 
-  function markerSize(earthquakes) {
-    return earthquakes / 40;
-  }
-
   // Define a baseMaps object to hold our base layers
   var baseMaps = {
     "Street Map": streetmap,
@@ -70,6 +79,7 @@ function createMap(earthquakes) {
     zoom: 5,
     layers: [streetmap, earthquakes]
   });
+
 
   // Create a layer control
   // Pass in our baseMaps and overlayMaps
